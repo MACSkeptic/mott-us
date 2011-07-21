c3ca6e47a77ace1f02f7fa90d019b0177ce275a6
MACSkeptic <macskeptic@gmail.com>
2011-07-21 12:25:37 -0300

    added a method to tell if a cell is occupied by a specific player

--- spec/motta/board-spec.js
+++ spec/motta/board-spec.js
@@ -1,6 +1,12 @@
 module("board");
 
 function $pos(specs) { return motta.position(specs); }
+function $pla(color) { 
+  return motta.player({
+    'name': 'Name of ' + color,
+    'color': color
+  }); 
+}
 
 test("all inside", function () {
   var board = motta.board(3);
@@ -45,7 +51,24 @@ test("occupy", function () {
   board.occupy(0, 0, "player one");
 
   ok(!board.cellAt(0, 0).empty());
+  ok(board.occupied(0, 0));
   ok(board.cellAt(1, 0).empty());
+  ok(!board.occupied(1, 0));
   ok(board.cellAt(0, 1).empty());
+  ok(!board.occupied(0, 1));
   ok(board.cellAt(1, 1).empty());
+  ok(!board.occupied(1, 1));
+});
+
+test("occupied by", function () {
+  var board = motta.board(2),
+      red = $pla('red'),
+      blue = $pla('blue');
+
+  board.occupy(1, 1, red);
+
+  ok(board.occupiedBy(1, 1, red));
+  ok(!board.occupiedBy(1, 1, blue));
+  ok(!board.occupiedBy(0, 0, blue));
+  ok(!board.occupiedBy(0, 0, red));
 });
