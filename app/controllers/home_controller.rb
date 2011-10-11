52d7b0448f37bc01c4ad297943dbd9d43173f809
MACSkeptic <macskeptic@gmail.com>
2011-10-11 10:30:36 -0400

    added name to users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    render :text => "hai _o/"
+    render :text => current_user.name
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
