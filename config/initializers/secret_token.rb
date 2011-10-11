be23b787bf0cff33b66a8c2727ce3e1570a73114
MACSkeptic <macskeptic@gmail.com>
2011-10-11 10:01:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QaSuperChallenge::Application.config.secret_token = '957131d75944e0401284bc9b59f9635ecc9202a408d707d5e165d958d706fcd787cf1a94fc56cee45440e18b2f95887ebf88c965a34d0bb7ea07bd40cd6be962'
