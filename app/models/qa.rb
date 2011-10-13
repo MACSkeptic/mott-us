4720f400c9655f7e1bbceab2d70ce24d83ff12e7
macskeptic <macskeptic@gmail.com>
2011-10-13 08:05:30 -0400

    validating some  stuff for new qas

--- app/models/qa.rb
+++ app/models/qa.rb
@@ -1,6 +1,18 @@
 class Qa < ActiveRecord::Base
   belongs_to :user
 
+  EMAIL_REGEXP = %r!^[\w._]+@[\w_]+\.([\w_]{3,10})(\.([\w_]{2,3})){0,1}$!
+
+  validates_presence_of :name
+  validates_presence_of :email
+
+  validates_uniqueness_of :name
+  validates_uniqueness_of :email
+
+  validates_format_of :email, 
+    :with => EMAIL_REGEXP, 
+    :message => "is invalid"
+
   def interests
     (self.interests_as_string.nil? || self.interests_as_string.empty?) ?
       [] : self.interests_as_string.split('interesting')
